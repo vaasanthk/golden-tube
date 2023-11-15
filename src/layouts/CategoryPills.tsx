2acbc65582daec17ab5b376f0c68bb5da7286564
Vasi <vasanth.3656@yahoo.com>
2023-11-15 23:16:12 +0530

    first commit

--- src/layouts/CategoryPills.tsx
+++ src/layouts/CategoryPills.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronRight, Container } from "lucide-react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
 import Button from "../components/Button"
 import { useEffect, useRef, useState } from "react"
 
